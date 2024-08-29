36415277a2a4bec1bc349ee811de728316b170da
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-08-29 23:06:32 +0200

    Headers fix for forwarding over HTTPS

--- webhook.py
+++ webhook.py
@@ -3,9 +3,11 @@ import os
 import sys
 import argparse
 import requests
-from flask import Flask, request, jsonify, make_response
+import logging
+import logging.handlers
+from flask import Flask, request, jsonify, make_response, abort
 
-
+LOG_LEVEL = logging.INFO
 app = Flask(__name__)
 
 try:
@@ -70,12 +72,27 @@ def ping():
 @app.route('/', methods=['POST'])
 def webhook_handler():
     jenkins_webhook_url = f'{jenkins_url}/github-webhook/'
+    logging.info(f'Forwarding to Jenkins URL: {jenkins_webhook_url}')
+
+    headers = {
+        'Content-Type': 'application/json',
+        'X-GitHub-Event': request.headers.get('X-GitHub-Event', ''),
+        'X-GitHub-Delivery': request.headers.get('X-GitHub-Delivery', ''),
+        'X-GitHub-Hook-ID': request.headers.get('X-GitHub-Hook-ID', ''),
+        'X-GitHub-Hook-Installation-Target-ID': request.headers.get('X-GitHub-Hook-Installation-Target-ID', ''),
+        'X-GitHub-Hook-Installation-Target-Type': request.headers.get('X-GitHub-Hook-Installation-Target-Type', ''),
+        'User-Agent': request.headers.get('User-Agent', 'GitHub-Hookshot')
+    }
 
     r = requests.post(
         jenkins_webhook_url,
         json=request.get_json(),
-        headers=request.headers
+        headers=headers
     )
+
+    if r.status_code == 404:
+        logging.error(r.text)
+        abort(404)
 
     if not r.status_code == 200:
         return make_response(jsonify(
@@ -92,6 +109,15 @@ def webhook_handler():
     ), 200)
 
 
+def setup_logging():
+    logging.getLogger().setLevel(LOG_LEVEL)
+    formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(stream_handler)
+
+
 if __name__ == '__main__':
     args = get_args()
+    setup_logging()
     app.run(host=args.host, port=args.port)
